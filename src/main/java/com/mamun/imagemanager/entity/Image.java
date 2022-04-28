438e68bfcc580ef167457b6cf312dfb54e8a6d55
mohamedmamun <mohamed.mamun@intelygenz.com>
2022-04-28 15:39:42 +0200

    Create Image entity

---
+++ src/main/java/com/mamun/imagemanager/entity/Image.java
@@ -0,0 +1,47 @@
+package com.mamun.imagemanager.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "image")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Timestamp timestamp;
+    private String imageBinario;
+    private String resolucion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getImageBinario() {
+        return imageBinario;
+    }
+
+    public void setImageBinario(String imageBinario) {
+        this.imageBinario = imageBinario;
+    }
+
+    public String getResolucion() {
+        return resolucion;
+    }
+
+    public void setResolucion(String resolucion) {
+        this.resolucion = resolucion;
+    }
+}
