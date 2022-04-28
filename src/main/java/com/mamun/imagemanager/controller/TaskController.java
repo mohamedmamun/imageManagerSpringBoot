a30d3f836b03997dbab7a9cc7e83d8576d22a3d0
mohamedmamun <mohamed.mamun@intelygenz.com>
2022-04-28 10:20:32 +0200

    Create task post endpoint

--- src/main/java/com/mamun/imagemanager/controller/TaskController.java
+++ src/main/java/com/mamun/imagemanager/controller/TaskController.java
@@ -1,10 +1,16 @@
 package com.mamun.imagemanager.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/api/v1")
 public class TaskController {
+
+    @PostMapping("/task")
+    public String post(){
+        return "task POST method";
+    }
 }
