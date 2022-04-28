8de3ed962f195c6ab8f4ce6ed8c322530fc152e8
mohamedmamun <mohamed.mamun@intelygenz.com>
2022-04-28 10:13:07 +0200

    Create project

---
+++ src/main/java/com/mamun/imagemanager/ImagemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mamun.imagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagemanagerApplication.class, args);
+	}
+
+}
