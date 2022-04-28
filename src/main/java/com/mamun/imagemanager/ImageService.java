47c8dc3ded3f44fee338008740bd8dd91a104e35
mohamedmamun <mohamed.mamun@intelygenz.com>
2022-04-28 17:59:35 +0200

    Create service class for image

--- src/main/java/com/mamun/imagemanager/ImageService.java
+++ src/main/java/com/mamun/imagemanager/ImageService.java
@@ -1,2 +1,25 @@
-package com.mamun.imagemanager;public class ImageService {
+package com.mamun.imagemanager;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class ImageService {
+
+    public void uploadImage(MultipartFile file){
+        try {
+            byte [] data = file.getBytes();
+            Path path = Paths.get("C:/Users/mohamed.mamun/Documents/imagemanager", file.getOriginalFilename());
+            Files.write(path,data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/com/mamun/imagemanager/controller/ImageController.java
+++ src/main/java/com/mamun/imagemanager/controller/ImageController.java
@@ -2,7 +2,9 @@ package com.mamun.imagemanager.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1")
